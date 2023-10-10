0bf8ce5cd7b800d7f0b8f2ffcfaed1b882bdf7d2
Jake Wardell (RIT Student) <jlw6563@rit.edu>
2023-10-10 19:44:29 -0400

    Player Movement / tilemap
    Added player game object, grid/tilemap, Manager. Added player movement functionality to the player movement script. Added empty manager script. We are using a capsule for the player and have some tiles added in to act as temp tiles until tiles are done and stuff.

--- Barber Shop 1992/Assets/Scripts/PlayerMovement.cs
+++ Barber Shop 1992/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    float speed;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Will move the player to left multiplyed by speed and delta time
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        //For right movement
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        //For up movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        //For down movement
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
     }
 }
