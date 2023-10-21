fe30d96dd1c2ac2932b4d80f726122173320239d
Jak3M <129016765+Jak3M@users.noreply.github.com>
2023-10-21 14:01:34 -0400

    We have circle movement

---
+++ Barber Shop 1992/Assets/circleMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using System.Threading;
+using UnityEngine;
+
+public class circleMovement : MonoBehaviour
+{
+
+    float speed = 5f;
+
+    float timer = 0;
+    Vector3 direction = Vector3.left;
+    
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Left End")
+        {
+            direction = Vector3.right;
+        }
+        if (collision.gameObject.name == "Right End")
+        {
+            direction = Vector3.left;
+        }
+    }
+}
