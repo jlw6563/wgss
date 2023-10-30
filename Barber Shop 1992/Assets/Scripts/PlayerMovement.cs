3024f8ee2489b05fa0bd558340a6ac11d1e26c53
Owen Beck (RIT Student) <oab1881@rit.edu>
2023-10-29 20:30:53 -0400

    Added box colliders and rigidbodies to all objects
    Collisions don't actually work, Jake could u fix please

--- Barber Shop 1992/Assets/Scripts/PlayerMovement.cs
+++ Barber Shop 1992/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     float speed;
 
+    //We should try normalizing movement to make sure player speed is the same when 2 keys are pressed
 
     // Update is called once per frame
     void Update()
@@ -14,22 +15,23 @@ public class PlayerMovement : MonoBehaviour
         //Will move the player to left multiplyed by speed and delta time
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.left * speed * Time.deltaTime;
+            transform.position += Vector3.left.normalized * speed * Time.deltaTime;
         }
         //For right movement
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            transform.position += Vector3.right.normalized * speed * Time.deltaTime;
         }
         //For up movement
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.up * speed * Time.deltaTime;
+            transform.position += Vector3.up.normalized * speed * Time.deltaTime;
+
         }
         //For down movement
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.down * speed * Time.deltaTime;
+            transform.position += Vector3.down.normalized * speed * Time.deltaTime;
         }
     }
 }
