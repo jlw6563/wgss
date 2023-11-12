ac64e1719a542bad9a1d32f3f65e170eb100619a
Jake Wardell (RIT Student) <jlw6563@rit.edu>
2023-11-12 15:06:15 -0500

    Changed Tile Maps and Fixed Quick Time Event
    Made it so the tile map has different sizes depending on whether it is walls or floor. Also changed quick time event to be better and more efficient.

--- Barber Shop 1992/Assets/Scripts/QuickTimeManager.cs
+++ Barber Shop 1992/Assets/Scripts/QuickTimeManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class QuickTimeManager : MonoBehaviour
@@ -8,34 +9,45 @@ public class QuickTimeManager : MonoBehaviour
     //Transform newTransform;
 
     [SerializeField]
-    GameObject goodZone;
+    Collider2D goodZone;
 
     [SerializeField]
-    GameObject circle;
+    Collider2D circle;
 
+    [SerializeField]
+    GameObject axis;
+
+    float translationVal;
+
+    /*
     //Min and Max
     float minGoodZone;
     float maxGoodZone;
     float minCircle;
     float maxCircle;
+    */
 
     // Start is called before the first frame update
     void Start()
     {
+        /*
         minGoodZone = 0 - (goodZone.transform.localScale.x / 2);
         maxGoodZone = 0 + (goodZone.transform.localScale.x / 2);
         minCircle = circle.transform.position.x - (circle.transform.localScale.x / 2);
         maxCircle = circle.transform.position.x + (circle.transform.localScale.x / 2);
+        */
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         minGoodZone = 0 - (goodZone.transform.localScale.x / 2);
         maxGoodZone = 0 + (goodZone.transform.localScale.x / 2);
         minCircle = circle.transform.position.x - (circle.transform.localScale.x / 2);
         maxCircle = circle.transform.position.x + (circle.transform.localScale.x / 2);
-
+        */
         //Debug.Log(minGoodZone);
 
         CollisionCheck();
@@ -44,24 +56,30 @@ public class QuickTimeManager : MonoBehaviour
 
     void CollisionCheck()
     {
-        /*
-        if(goodZoneCollider.bounds.Contains(circle) && Input.GetKey(KeyCode.Space)) 
+        
+        if(goodZone.bounds.Contains(circle.bounds.max) && goodZone.bounds.Contains(circle.bounds.min) && Input.GetKeyDown(KeyCode.Space)) 
         {
             Debug.Log("Collsioj");
-            circle.x = Random.Range(-1.3f, 1.3f);
-        }
-        */
 
+            translationVal = axis.transform.localScale.x / 2 - goodZone.transform.localScale.x / 2;
+            goodZone.gameObject.transform.position =  new Vector3 (Random.Range(-translationVal, translationVal),goodZone.gameObject.transform.position.y);
+        }else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Miss");
+        }
+        
+        /*
         if(minGoodZone <= minCircle && maxGoodZone >= maxCircle && Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("jake's sexy face");
-            goodZone.transform.localScale = new Vector3(Random.Range(0.4f, 2f), 0.5f, 1f);
+            goodZone.transform.localScale = new Vector3(Random.Range(0.4f, 2f),goodZone.transform.localScale.y, 1f);
             //goodZone.transform.position = new Vector3(Random.Range(-1.3f, 1.3f), 0f, 1f);
         }
         else if(Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("ignacio dirty dog eater");
         }
+        */
     }
 
 }
